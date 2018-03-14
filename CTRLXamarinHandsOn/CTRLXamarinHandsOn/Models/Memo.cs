ed176bd501dd4342b3e966d3781695254c428b83
poketorena <poketorena@users.noreply.github.com>
2018-03-14 13:31:38 +0900

    Idプロパティのアクセスをgetのみに修正

--- CTRLXamarinHandsOn/CTRLXamarinHandsOn/Models/Memo.cs
+++ CTRLXamarinHandsOn/CTRLXamarinHandsOn/Models/Memo.cs
@@ -6,7 +6,7 @@ namespace CTRLXamarinHandsOn.Models
     public class Memo : BindableBase
     {
         // プロパティ
-        public string Id { get; set; }
+        public string Id { get; }
 
         private string _title;
         public string Title
